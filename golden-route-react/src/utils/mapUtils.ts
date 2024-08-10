4414b5b9ba43a8feed54cd4ae4ffd976d74ef5cb
3liorY <eliory@checkpoint.com>
2024-08-10 21:40:28 +0300

    Delete mapUtils.ts

--- golden-route-react/src/utils/mapUtils.ts
+++
@@ -1,6 +0,0 @@
-export async function initMap(centerLat: number, centerLon: number): Promise<void> {
-  new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    center: { lat: centerLat, lng: centerLon },
-    zoom: 10
-  });
-}
